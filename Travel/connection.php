376e39ff2ddbe15b548a86f6db7405167ff1c607
Sharvari Sunil  Shinde <135091984+ShindeSharvari@users.noreply.github.com>
2025-03-04 12:39:49 +0530

    Delete Travel directory

--- Travel/connection.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    $con = mysqli_connect('localhost','root','','travel');
-    if(!$con)
-    {
-        echo 'please check your Database connection';
-    }
-?>
