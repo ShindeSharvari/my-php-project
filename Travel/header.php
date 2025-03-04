de8fe1ab552649394c60fe4fae8881a4e56388ce
Sharvari Sunil  Shinde <135091984+ShindeSharvari@users.noreply.github.com>
2025-03-04 13:19:32 +0530

    Add files via upload

---
+++ Travel/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SHIV TRAVELS</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.4.0/fonts/remixicon.css" rel="stylesheet">
+</head>
+<body>
+<nav class="nav">
+    <div class="nav__logo">
+        <a href="#"><img src="images/logo.png" alt="logo" /></a>
+    </div>
+    <ul class="nav__links">
+        <li class="link"><a href="#">Home</a></li>
+        <li class="link"><a href="#">About Us</a></li>
+        <li class="link"><a href="#">Destination</a></li>
+        <li class="link"><a href="#">Package</a></li>
+        <li class="link"><a href="#">Gallery</a></li>
+        <li class="link"><a href="#">Contact Us</a></li>
+    </ul>
+    <a href="register.php">
+    <button class="btn blue">JOIN US</button>
+    </a>
+</nav>
+</body>
+</html>
